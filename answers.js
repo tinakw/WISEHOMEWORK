872e60bcbc297f409cd51d88a339fc151e7ea4f5
tinakw <110048198+tinakw@users.noreply.github.com>
2022-08-15 16:55:11 -0400

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -50,7 +50,34 @@
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+ 
+// class WildLife {
+//     constructor(name, species,age, hometown ){
+//         this.name = name;
+//         this.species = species;
+//         this.age = age;
+//         this.hometown = hometown
+//     }
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee.splice(2,1,5001);
+console.log(plantee);
+
+wolfy.splice(3,1,'Gotham City');
+console.log(wolfy)
+
+dart.push('Hawkins');
+console.log(dart)
+
+wolfy.splice(0,1,'Gameboy')
+console.log(wolfy)
+
+//The commit message should read: "Wild Wild Life answered"
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
