9342e020d765e95eb0c9beca54be2a15ac67ccd3
tinakw <110048198+tinakw@users.noreply.github.com>
2022-08-15 16:06:11 -0400

    Fizz Buzz answered

---
+++ answers.js
@@ -0,0 +1,77 @@
+////////////////////////////////
+// Easy Going
+////////////////////////////////
+// for (let i = 0; i <= 20; i++){
+//     console.log(i)
+// }
+
+//The commit message should read: "Easy Going answered"
+
+
+
+////////////////////////////////
+// Get Even
+////////////////////////////////
+
+// for (let i = 0; i <= 200; i++){
+//    if( i % 2 ===0){
+//     console.log(`${i} <--- is an even number`)
+//    } 
+// }
+
+//The commit message should read: "Get Even answered"
+
+////////////////////////////////
+// Fizz Buzz
+////////////////////////////////
+
+// function fizzBuzz(){
+
+// let num = [];
+// for (let i = 0; num.length <= 100; i++){
+//         if( i % 3 === 0 && i % 5 === 0){
+//         num.push('FizzBuzz')
+//     } else if (i % 3 === 0){
+//         num.push('Fizz')
+//     } else if (i % 5 === 0){
+//         num.push('Buzz')
+//     }
+//         else{
+//             num.push(i)
+//         }
+// }
+// return num
+// }
+
+// console.log(fizzBuzz())
+   
+ //The commit message should read: "Fizz Buzz answered"  
+        
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+////////////////////////////////
+
+
+////////////////////////////////
+// Methods, Revisited
+////////////////////////////////
+
+
+////////////////////////////////
+// Where is Waldo
+////////////////////////////////
+
+
+////////////////////////////////
+//  Excited Kitten
+////////////////////////////////
+
+
+////////////////////////////////
+//  Find the Median
+////////////////////////////////
